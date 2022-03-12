46f494c2b89ce1bc7b9c30798e064e7ad35f40d4
Kazuki Taninaka <onelive555@gmail.com>
2022-03-12 23:26:44 +0900

    feat: enable creating new event and share via line

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,15 +1,19 @@
 import React, { useState, useRef } from 'react';
-import { Input, Text, Button, Box, Flex } from '@chakra-ui/react';
+import { Input, Text, Button, Box, Flex, Center } from '@chakra-ui/react';
 import { CloseIcon } from '@chakra-ui/icons';
 import { useInsertEventMutation } from '../generated/graphql';
 import { NextPage } from 'next';
+import useLiff from '../hooks/useLiff';
 
 const Create: NextPage = () => {
   const [participants, setParticipants] = useState<{ name: string }[]>([
     { name: '' },
   ]);
   const [eventName, setEventName] = useState<string>('');
+  const [isEventCreated, setIsEventCreated] = useState<boolean>(false);
+  const [eventId, setEventId] = useState<string>('');
   const [insertEvent, { loading: isInserting }] = useInsertEventMutation();
+  const { liff } = useLiff();
 
   const addParticipant = () => {
     setParticipants((prev) => [...prev, { name: '' }]);
@@ -33,19 +37,45 @@ const Create: NextPage = () => {
     });
   };
 
-  const onShare = () => {
+  const handleCreateEventClick = () => {
     insertEvent({
       variables: {
         eventName,
         participants: participants.filter((participant) => participant.name), // nameが空のものは除く
       },
-    });
-    alert('success');
+    })
+      .then((res) => {
+        setIsEventCreated(true);
+        setEventId(res.data?.insert_events_one?.id);
+      })
+      .catch(() => {
+        throw new Error('Error: Could not create a new event.');
+      });
+  };
+
+  const handleShareClick = () => {
+    if (!liff) return;
+    if (liff.isApiAvailable('shareTargetPicker')) {
+      console.log('hoge');
+      liff
+        .shareTargetPicker(
+          [
+            {
+              type: 'text',
+              text: `割り勘イベント「${eventName}」が作成されました！\n以下のリンクから支払いを追加していきましょう！\n${process.env.NEXT_PUBLIC_DEV_LINK}/event/${eventId}`,
+            },
+          ],
+          { isMultiple: true },
+        )
+        .then((res) => {
+          console.log(res);
+        });
+    }
   };
 
   return (
     <div>
-      <Text fontSize="lg" fontWeight="bold">
+      <Text fontSize="lg" fontWeight="bold" mb="2">
         新規割り勘イベント作成
       </Text>
       <Input
@@ -53,33 +83,53 @@ const Create: NextPage = () => {
         value={eventName}
         onChange={(e) => setEventName(e.target.value)}
         required
-        mt="2"
+        mb="5"
       ></Input>
-      <Text fontSize="lg" fontWeight="bold" mt="5">
+      <Text fontSize="lg" fontWeight="bold" mb="2">
         割り勘参加者
       </Text>
-      {participants.map((participant, index) => (
-        <Flex columnGap="3" alignItems="center" key={`participant-${index}`}>
-          <Input
-            placeholder="参加者名"
-            value={participant.name}
-            onChange={(e) => setParticipantName(index, e)}
-            mt={2}
-            autoFocus
-          />
-          <CloseIcon mt="2" mr="2" onClick={() => deleteParticipant(index)} />
-        </Flex>
-      ))}
-      <Box textAlign="center">
-        <Button mt="3" onClick={addParticipant}>
-          参加者を追加
-        </Button>
+      <Box mb="3">
+        {participants.map((participant, index) => (
+          <Flex columnGap="3" alignItems="center" key={`participant-${index}`}>
+            <Input
+              placeholder="参加者名"
+              value={participant.name}
+              onChange={(e) => setParticipantName(index, e)}
+              autoFocus
+              mb="2"
+            />
+            <CloseIcon mr="2" onClick={() => deleteParticipant(index)} />
+          </Flex>
+        ))}
       </Box>
-      <Box textAlign="center" mt="10">
-        <Button bgColor="green.400" color="white" onClick={onShare}>
-          {isInserting ? 'イベント作成中...' : 'グループに共有'}
-        </Button>
+      <Box textAlign="center" mb="10">
+        <Button onClick={addParticipant}>参加者を追加</Button>
       </Box>
+      <Center mb="3">
+        <Button
+          colorScheme="teal"
+          onClick={handleCreateEventClick}
+          disabled={isInserting || isEventCreated}
+        >
+          {isInserting
+            ? 'イベント作成中...'
+            : isEventCreated
+            ? 'イベント作成完了'
+            : 'イベント作成'}
+        </Button>
+      </Center>
+      <Center mb="3">
+        <Text>イベントを作成したらグループのみんなにリンクを共有しよう</Text>
+      </Center>
+      <Center>
+        <Button
+          colorScheme="green"
+          onClick={handleShareClick}
+          disabled={!isEventCreated}
+        >
+          LINEでグループに共有
+        </Button>
+      </Center>
     </div>
   );
 };
