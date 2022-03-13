a673b5ac5a1c07444b21c6ccad0723fe0dfe2946
Kazuki Taninaka <onelive555@gmail.com>
2022-03-13 21:13:34 +0900

    feat: AddWarikankun

---
+++ src/components/AddWarikankun.tsx
@@ -0,0 +1,21 @@
+import { Box, Text, Button, Center } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import React from 'react';
+
+const AddWarikankun = () => {
+  const router = useRouter();
+  return (
+    <Box>
+      <Text mb="3" fontSize="sm">
+        割り勘くんと友達になって、
+        <br />
+        新規割り勘イベントを作成しよう！
+      </Text>
+      <Button onClick={() => router.push('https://lin.ee/Tkc57gX')}>
+        割り勘くんと友達になる
+      </Button>
+    </Box>
+  );
+};
+
+export default AddWarikankun;
