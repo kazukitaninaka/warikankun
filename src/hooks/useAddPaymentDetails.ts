0db9370896f1801cc4fedb65995f40c6d13c1e52
Kazuki Taninaka <onelive555@gmail.com>
2022-05-01 21:34:02 +0900

    add: useAddPaymentDetails.ts

---
+++ src/hooks/useAddPaymentDetails.ts
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { QueryParticipantsQuery } from '../generated/graphql';
+
+type Details = {
+  id: number;
+  name: string;
+  amount: string;
+}[];
+
+const useAddPaymentDetails = (
+  event: QueryParticipantsQuery['events'][number] | undefined,
+) => {
+  const [details, setDetails] = useState<Details>([]);
+
+  useEffect(() => {
+    if (!event) return;
+    const details = event.participants.map((participant) => {
+      return { id: participant.id, name: participant.name, amount: '' };
+    });
+    setDetails(details);
+  }, [event]);
+  return { details, setDetails };
+};
+
+export default useAddPaymentDetails;
