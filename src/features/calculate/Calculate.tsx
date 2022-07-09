475dc5e4f64b14a946e45645b39be6e321216710
Kazuki Taninaka <onelive555@gmail.com>
2022-07-09 16:10:37 +0900

    refactor: Calculating移動

--- src/features/calculate/Calculate.tsx
+++ src/features/calculate/Calculate.tsx
@@ -2,7 +2,7 @@ import { Box, Center, Button, Text } from '@chakra-ui/react';
 import { faShareSquare } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useRouter } from 'next/router';
-import Calculating from '@components/Calculating';
+import Calculating from '@features/calculate/Calculating';
 import EventName from '@components/EventName';
 import { liffVar } from '@components/LiffProvider';
 import SumPrice from '@components/SumPrice';
