4734730853b9275e8267d321773e213ed14af8ac
Kazuki Taninaka <onelive555@gmail.com>
2022-09-09 16:55:11 +0200

    update seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,11 +2,46 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const test = await prisma.event.create({
-    data: {
-      name: 'test event',
-    },
-  });
+  //   const test = await prisma.payment.create({
+  //     data: {
+  //       eventId: '4d13f0e5-a25a-4537-9590-b6ec98cfc4ac',
+  //       name: 'コンビニ',
+  //       amount: 1000,
+  //       whoPaidId: 1,
+  //     },
+  //   });
+  //   const test = await prisma.participant.createMany({
+  //     data: [
+  //       {
+  //         name: 'John',
+  //         eventId: '4d13f0e5-a25a-4537-9590-b6ec98cfc4ac',
+  //       },
+  //       {
+  //         name: 'Abby',
+  //         eventId: '4d13f0e5-a25a-4537-9590-b6ec98cfc4ac',
+  //       },
+  //       {
+  //         name: 'Tom',
+  //         eventId: '4d13f0e5-a25a-4537-9590-b6ec98cfc4ac',
+  //       },
+  //     ],
+  //   });
+
+  // const test = await prisma.payment.create({
+  //   data: {
+  //     eventId: '4d13f0e5-a25a-4537-9590-b6ec98cfc4ac',
+  //     name: 'コンビニ',
+  //     whoPaidId: 1,
+  //     amount: 3000,
+  //     whoShouldPay: {
+  //       create: [
+  //         { participantId: 1, ratio: 1 },
+  //         { participantId: 2, ratio: 1 },
+  //         { participantId: 3, ratio: 1 },
+  //       ],
+  //     },
+  //   },
+  // });
   console.log(test);
 }
 
