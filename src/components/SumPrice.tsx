c90322c6d4fae77b2c21d40c69e2fdb58d392d26
Kazuki Taninaka <onelive555@gmail.com>
2022-09-11 22:06:17 +0200

    fix query in SumPrice

--- src/components/SumPrice.tsx
+++ src/components/SumPrice.tsx
@@ -1,21 +1,19 @@
 import { Text } from '@chakra-ui/react';
-import { useQuerySumPriceQuery } from '@generated/deprecatedGraphql';
+import { useGetSumPriceQuery } from '@generated/graphql';
 import { formatNumberToJPY } from '../utils';
 
 const SumPrice = ({ id }: { id: string | string[] | undefined }) => {
-  const { loading, error, data } = useQuerySumPriceQuery({
-    variables: { eventId: id },
+  const { isLoading, isError, data } = useGetSumPriceQuery({
+    // TODO: asを消す
+    eventId: id as string,
   });
-  if (error) {
+  if (isError) {
     return <Text data-testid="errorText">データ取得に失敗しました。</Text>;
   }
   return (
     <Text data-testid="text">
       支払い総額：
-      {!loading &&
-        formatNumberToJPY(
-          data?.events[0].payments_aggregate.aggregate?.sum?.amount!,
-        )}
+      {!isLoading && formatNumberToJPY(data.event.sumPrice)}
     </Text>
   );
 };
