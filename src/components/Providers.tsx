ca6fc743aa03db2b5dd329c9491a7e0e73b1d6a0
Kazuki Taninaka <onelive555@gmail.com>
2023-10-09 14:58:05 +0900

    introduce ChakraBaseProvider to reduce the bundle size

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,19 +1,30 @@
 'use client';
 
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react';
+import { Button, Input, Heading, Table } from '@chakra-ui/theme/components';
 import { CacheProvider } from '@chakra-ui/next-js';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import LiffProvider from './LiffProvider';
 
+const theme = extendBaseTheme({
+  components: {
+    Button,
+    Input,
+    Heading,
+    Table,
+  },
+});
+
 const queryClient = new QueryClient();
+
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <CacheProvider>
-      <ChakraProvider>
+      <ChakraBaseProvider theme={theme}>
         <QueryClientProvider client={queryClient}>
           <LiffProvider>{children}</LiffProvider>
         </QueryClientProvider>
-      </ChakraProvider>
+      </ChakraBaseProvider>
     </CacheProvider>
   );
 };
