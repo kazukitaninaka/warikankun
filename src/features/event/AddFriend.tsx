8c190097dc9d56f5d9b3f092e310ab544f1c0849
Kazuki Taninaka <onelive555@gmail.com>
2023-12-08 18:28:04 +0900

    fix line friendship button style

--- src/features/event/AddFriend.tsx
+++ src/features/event/AddFriend.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box, Text, Button } from '@chakra-ui/react';
+import { Center, Text, Button, Box } from '@chakra-ui/react';
 import Link from 'next/link';
 import React from 'react';
 import useFriendship from '@hooks/useFriendship';
@@ -10,14 +10,18 @@ const AddFriend = () => {
   if (!isFriend) return null;
   return (
     <Box>
-      <Text mb="3" fontSize="sm">
-        割り勘くんと友達になって、
-        <br />
-        新規割り勘イベントを作成しよう！
-      </Text>
-      <Link href={'https://lin.ee/Tkc57gX'} passHref>
-        <Button>割り勘くんと友達になる</Button>
-      </Link>
+      <Center>
+        <Text mb="3" fontSize="sm">
+          割り勘くんと友達になって、
+          <br />
+          新規割り勘イベントを作成しよう！
+        </Text>
+      </Center>
+      <Center>
+        <Link href={'https://lin.ee/Tkc57gX'} passHref>
+          <Button>割り勘くんと友達になる</Button>
+        </Link>
+      </Center>
     </Box>
   );
 };
